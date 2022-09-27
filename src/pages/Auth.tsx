9cdb24389a4966c4ea03850d372878f6cc0f63d3
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2022-09-27 12:45:10 +0600

    Add forgot password functionality

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -5,12 +5,15 @@ import { useNavigate } from 'react-router-dom';
 import { ButtonCustom } from '@/components/ui/button-custom';
 import { useToast } from '@/hooks/use-toast';
 import { useEffect } from 'react';
+import { Mail } from 'lucide-react';
+import { supabase } from '@/integrations/supabase/client';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isSignUp, setIsSignUp] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [isForgotPassword, setIsForgotPassword] = useState(false);
   const { signIn, signUp, user } = useAuth();
   const navigate = useNavigate();
   const { toast } = useToast();
@@ -27,12 +30,20 @@ const Auth = () => {
     setLoading(true);
 
     try {
-      if (isSignUp) {
-        await signUp(email, password);
-        toast({
-          title: "Check your email",
-          description: "We've sent you a confirmation link.",
+      if (isForgotPassword) {
+        const { error } = await supabase.auth.resetPasswordForEmail(email, {
+          redirectTo: `${window.location.origin}/auth`,
         });
+        
+        if (error) throw error;
+        
+        toast({
+          title: "Reset link sent",
+          description: "Check your email for the password reset link.",
+        });
+        setIsForgotPassword(false);
+      } else if (isSignUp) {
+        await signUp(email, password);
       } else {
         await signIn(email, password);
         navigate('/');
@@ -49,12 +60,18 @@ const Auth = () => {
       <div className="bg-white rounded-lg shadow-xl p-8 w-full max-w-md">
         <div className="text-center mb-8">
           <h1 className="text-3xl font-bold text-gray-800 mb-2">
-            {isSignUp ? 'Create an Account' : 'Welcome Back'}
+            {isForgotPassword 
+              ? 'Reset Password'
+              : isSignUp 
+                ? 'Create an Account' 
+                : 'Welcome Back'}
           </h1>
           <p className="text-gray-600">
-            {isSignUp 
-              ? 'Sign up to start improving your speech' 
-              : 'Log in to continue your speech journey'}
+            {isForgotPassword
+              ? 'Enter your email to receive a reset link'
+              : isSignUp 
+                ? 'Sign up to start improving your speech' 
+                : 'Log in to continue your speech journey'}
           </p>
         </div>
 
@@ -74,21 +91,23 @@ const Auth = () => {
             />
           </div>
 
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-pink-500 focus:border-pink-500"
-              placeholder="••••••••"
-              minLength={6}
-            />
-          </div>
+          {!isForgotPassword && (
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-pink-500 focus:border-pink-500"
+                placeholder="••••••••"
+                minLength={6}
+              />
+            </div>
+          )}
 
           <ButtonCustom
             type="submit"
@@ -105,18 +124,30 @@ const Auth = () => {
                 Processing...
               </span>
             ) : (
-              isSignUp ? 'Sign Up' : 'Log In'
+              isForgotPassword ? 'Send Reset Link' : (isSignUp ? 'Sign Up' : 'Log In')
             )}
           </ButtonCustom>
         </form>
 
-        <div className="mt-6 text-center">
+        <div className="mt-6 text-center space-y-2">
+          {!isForgotPassword && (
+            <button
+              type="button"
+              onClick={() => setIsSignUp(!isSignUp)}
+              className="text-pink-600 hover:text-pink-800 text-sm font-medium"
+            >
+              {isSignUp ? 'Already have an account? Log In' : 'Need an account? Sign Up'}
+            </button>
+          )}
+          
           <button
             type="button"
-            onClick={() => setIsSignUp(!isSignUp)}
-            className="text-pink-600 hover:text-pink-800 text-sm font-medium"
+            onClick={() => setIsForgotPassword(!isForgotPassword)}
+            className="block w-full text-pink-600 hover:text-pink-800 text-sm font-medium"
           >
-            {isSignUp ? 'Already have an account? Log In' : 'Need an account? Sign Up'}
+            {isForgotPassword 
+              ? 'Back to login' 
+              : 'Forgot your password?'}
           </button>
         </div>
       </div>
