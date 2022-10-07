8fd21f48470a5657e680db510c93b841827fea56
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2022-10-07 22:53:03 +0600

    Update Welcome section content

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,6 +1,6 @@
 
 import { useEffect, useState, useRef } from 'react';
-import { MicIcon, SparklesIcon, BookOpenIcon, ClockIcon, GlobeIcon, HeartIcon } from 'lucide-react';
+import { Wand2Icon, FileTextIcon, UsersIcon, ClockIcon, CalendarIcon, CreditCardIcon } from 'lucide-react';
 
 interface FeatureProps {
   icon: JSX.Element;
@@ -77,34 +77,34 @@ const Features = () => {
 
   const features = [
     {
-      icon: <SparklesIcon className="h-6 w-6" />,
-      title: "AI-Powered Speech Magic",
-      description: "Our advanced AI analyzes your topic and audience to generate personalized speech suggestions."
+      icon: <Wand2Icon className="h-6 w-6" />,
+      title: "AI-Powered Magic",
+      description: "At Speech Help, we don't just write speeches – we help you create memorable moments! Our AI-powered system, SpeechHelp.ai, is like a personal speech wizard, crafting customized prompts that turn your ideas into a speech that will leave everyone talking."
     },
     {
-      icon: <BookOpenIcon className="h-6 w-6" />,
-      title: "The Perfect Speech Library",
-      description: "Choose from professionally crafted templates for any occasion or purpose."
+      icon: <FileTextIcon className="h-6 w-6" />,
+      title: "No More Writer's Block",
+      description: "Goodbye, blank page! Say farewell to staring at an empty screen, wondering where to start. With Speech Help, you're never at a loss for words. Our AI is always ready to spark your creativity and guide you past writer's block, one prompt at a time."
     },
     {
-      icon: <MicIcon className="h-6 w-6" />,
-      title: "Practice Makes Perfect",
-      description: "Receive coaching on tone, pace, and emphasis to deliver with confidence."
+      icon: <UsersIcon className="h-6 w-6" />,
+      title: "Tailored Just For You",
+      description: "Whether you're making a toast, delivering a tribute, or crafting a speech for an important event, our prompts are custom-tailored to fit your needs, occasion, and mood. Humor? Sentimentality? We've got you covered."
     },
     {
       icon: <ClockIcon className="h-6 w-6" />,
-      title: "Create Speeches On Time",
-      description: "Create compelling speeches in minutes instead of hours with our intuitive AI."
+      title: "Create Speeches in No Time",
+      description: "Why stress over speeches for days when you can create impactful speeches in minutes? Our intuitive interface lets you generate, edit, and personalize your speech without hassle. Whether you're a pro or a first-time speaker, we make the process easy and fast."
     },
     {
-      icon: <GlobeIcon className="h-6 w-6" />,
-      title: "The Perfect Structure",
-      description: "Generate and edit speeches in multiple languages to connect with diverse audiences."
+      icon: <CalendarIcon className="h-6 w-6" />,
+      title: "For Every Occasion",
+      description: "From weddings to corporate events to personal speeches, Speech Help adapts to all occasions. We're ready to help you prepare speeches that range from heartfelt to hilarious, and everything in between."
     },
     {
-      icon: <HeartIcon className="h-6 w-6" />,
-      title: "The Emotional Connection",
-      description: "Our AI understands emotional context, helping craft speeches that genuinely resonate."
+      icon: <CreditCardIcon className="h-6 w-6" />,
+      title: "No Subscription Hassles",
+      description: "Get started without a commitment. Try Speech Help with no pressure to buy. We believe in letting you see the value before making any decisions."
     }
   ];
 
@@ -116,7 +116,7 @@ const Features = () => {
             Welcome to <span className="text-pink-600">Speech Help!</span>
           </h2>
           <p className={`text-lg text-gray-600 mb-6 opacity-0 ${isVisible ? 'animate-fade-in stagger-1' : ''}`}>
-            Our award-winning tool makes speech creation simple and stress-free. With cutting-edge AI technology, we're transforming public speaking help, making it accessible to everyone.
+            Ever stared at a blank page and felt it judging you? Whether you're giving a wedding toast, rallying a team, or delivering a conference keynote, Speech Help is here to rescue you from idea paralysis and blank-page syndrome.
           </p>
         </div>
 
