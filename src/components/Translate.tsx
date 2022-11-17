02207c6c8c1642f69e17ac11aa7fa71170c3e353
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2022-11-17 17:02:57 +0600

    Add missing speech translations

--- src/components/Translate.tsx
+++ src/components/Translate.tsx
@@ -4,17 +4,21 @@ import { useTranslation } from '@/translations';
 
 interface TranslateProps {
   text: string;
+  fallback?: string;
 }
 
-const Translate: React.FC<TranslateProps> = ({ text }) => {
+const Translate: React.FC<TranslateProps> = ({ text, fallback }) => {
   const { currentLanguage } = useLanguage();
   const { t } = useTranslation();
   
   const translation = t(text, currentLanguage.code);
   
   // If the translation is the same as the key and it looks like a translation key,
-  // render it in a more user-friendly way
+  // render the fallback text if provided or format the key in a user-friendly way
   if (translation === text && text.includes('.')) {
+    if (fallback) {
+      return <>{fallback}</>;
+    }
     const parts = text.split('.');
     return <>{parts[parts.length - 1].replace(/([A-Z])/g, ' $1').trim()}</>;
   }
