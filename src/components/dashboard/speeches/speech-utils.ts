3c2a855c4352d3f643b75112b1f2910650402dd6
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2022-11-20 13:24:57 +0600

    Add missing speech types

--- src/components/dashboard/speeches/speech-utils.ts
+++ src/components/dashboard/speeches/speech-utils.ts
@@ -14,6 +14,11 @@ export const getSpeechTypeLabel = (type: string) => {
     'keynote': 'Keynote Address',
     'social': 'Social Speech',
     'farewell': 'Farewell Speech',
+    'informative': 'Informative Speech',
+    'persuasive': 'Persuasive Speech',
+    'entertaining': 'Entertaining Speech',
+    'retirement': 'Retirement Speech',
+    'award': 'Award Ceremony Speech',
     'other': 'Other Speech'
   };
   
@@ -32,6 +37,11 @@ export const getTypeColor = (type: string) => {
     'keynote': 'bg-emerald-100 text-emerald-800',
     'social': 'bg-indigo-100 text-indigo-800',
     'farewell': 'bg-cyan-100 text-cyan-800',
+    'informative': 'bg-teal-100 text-teal-800',
+    'persuasive': 'bg-orange-100 text-orange-800',
+    'entertaining': 'bg-violet-100 text-violet-800',
+    'retirement': 'bg-sky-100 text-sky-800',
+    'award': 'bg-lime-100 text-lime-800',
     'other': 'bg-gray-100 text-gray-800'
   };
   
