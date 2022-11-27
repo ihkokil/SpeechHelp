c0c9f29cfcb9b696eff28d61f15c2a0bc8aef31f
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2022-11-27 17:32:36 +0600

    Generate and display speech content

--- src/components/speech/SpeechLabContent.tsx
+++ src/components/speech/SpeechLabContent.tsx
@@ -13,7 +13,8 @@ import { useTranslation } from '@/translations';
 const SpeechLabContent: React.FC = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [selectedSpeechType, setSelectedSpeechType] = useState('');
-  const [speechDetails, setSpeechDetails] = useState({});
+  const [formData, setFormData] = useState<Record<string, string>>({});
+  const [generatedSpeech, setGeneratedSpeech] = useState('');
   const { currentLanguage } = useLanguage();
   const { t } = useTranslation();
   
@@ -135,6 +136,11 @@ const SpeechLabContent: React.FC = () => {
   const nextStep = () => {
     if (currentStep < 4) {
       setCurrentStep(currentStep + 1);
+      
+      // Generate speech content when moving from step 3 to 4
+      if (currentStep === 3) {
+        generateSpeechContent();
+      }
     }
   };
 
@@ -142,6 +148,83 @@ const SpeechLabContent: React.FC = () => {
     if (currentStep > 1) {
       setCurrentStep(currentStep - 1);
     }
+  };
+  
+  // Function to generate speech content based on form data
+  const generateSpeechContent = () => {
+    const speechType = speechTypes.find(type => type.id === selectedSpeechType)?.label || '';
+    const speaker = formData["Who are you in relation to the wedding?"] || 
+                   formData["Your role in the event?"] || 
+                   formData["Your name and relationship to the celebrant?"] ||
+                   formData["Your name, title, and company?"] ||
+                   "the speaker";
+    
+    const name = formData["What is your name?"] || "";
+    const relationship = formData["What is your relationship to the couple?"] || 
+                         formData["Your relationship to the retiree?"] || 
+                         formData["Your relationship to the celebrant?"] || "";
+    
+    const introduction = name ? 
+      `Good evening, my name is ${name}${relationship ? `, ${relationship}` : ""}.` : 
+      "Good evening, everyone.";
+    
+    // Create a personalized speech intro based on the type
+    let speech = `${introduction}\n\n`;
+    
+    // Add appropriate content based on speech type
+    switch (selectedSpeechType) {
+      case 'wedding':
+        speech += `I am honored to stand before you today as ${speaker} to celebrate the union of this wonderful couple.\n\n`;
+        if (formData["Share a memorable story about the couple."]) {
+          speech += `I'd like to share a special memory: ${formData["Share a memorable story about the couple."]}\n\n`;
+        }
+        if (formData["Qualities you admire about the bride/groom."]) {
+          speech += `What I admire most about them is ${formData["Qualities you admire about the bride/groom."]}\n\n`;
+        }
+        speech += "I wish you both a lifetime of love, joy, and happiness. Congratulations!\n\n";
+        if (formData["End with a toast?"]) {
+          speech += `So let's raise our glasses: ${formData["End with a toast?"]}\n`;
+        } else {
+          speech += "So let's raise our glasses to the newlyweds! To love, laughter, and happily ever after!";
+        }
+        break;
+        
+      case 'graduation':
+        speech += `It's a privilege to address you today at this significant milestone.\n\n`;
+        if (formData["Key message or theme?"]) {
+          speech += `Today, I want to focus on ${formData["Key message or theme?"]}\n\n`;
+        }
+        if (formData["Share a personal story or experience."]) {
+          speech += `${formData["Share a personal story or experience."]}\n\n`;
+        }
+        speech += "As you move forward from here, remember that this is not the end, but the beginning of a new chapter.\n\n";
+        if (formData["Is there a call to action or advice?"]) {
+          speech += `My advice to you is: ${formData["Is there a call to action or advice?"]}\n\n`;
+        }
+        speech += "Congratulations, graduates! The world awaits your contributions.";
+        break;
+        
+      default:
+        // Generic speech content
+        speech += `I'm delighted to speak to you today.\n\n`;
+        
+        // Add any personal stories if provided
+        const storyKey = Object.keys(formData).find(key => key.toLowerCase().includes("story") || key.toLowerCase().includes("experience"));
+        if (storyKey && formData[storyKey]) {
+          speech += `${formData[storyKey]}\n\n`;
+        }
+        
+        // Add any qualities or achievements if provided
+        const qualitiesKey = Object.keys(formData).find(key => key.toLowerCase().includes("qualities") || key.toLowerCase().includes("achievements"));
+        if (qualitiesKey && formData[qualitiesKey]) {
+          speech += `${formData[qualitiesKey]}\n\n`;
+        }
+        
+        // Generic closing
+        speech += "Thank you for your attention and time today.";
+    }
+    
+    setGeneratedSpeech(speech);
   };
 
   // Update the steps for the progress indicator to match our flow
@@ -173,6 +256,7 @@ const SpeechLabContent: React.FC = () => {
               nextStep={nextStep} 
               prevStep={prevStep}
               selectedSpeechType={selectedSpeechType}
+              setFormData={setFormData}
             />
           )}
 
@@ -182,12 +266,15 @@ const SpeechLabContent: React.FC = () => {
               prevStep={prevStep} 
               selectedSpeechType={selectedSpeechType}
               speechTypes={speechTypes}
+              formData={formData}
             />
           )}
 
           {currentStep === 4 && (
             <Step4EditSpeech 
               prevStep={prevStep}
+              generatedSpeech={generatedSpeech}
+              speechTitle={formData["speechTitle"] || "My Speech"}
             />
           )}
         </div>
--- src/components/speech/steps/Step3GenerateSpeech.tsx
+++ src/components/speech/steps/Step3GenerateSpeech.tsx
@@ -1,14 +1,16 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { ButtonCustom } from '@/components/ui/button-custom';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
-import { ArrowLeft } from 'lucide-react';
+import { ArrowLeft, PartyPopper, Sparkles } from 'lucide-react';
 import { useLanguage } from '@/contexts/LanguageContext';
 import { useTranslation } from '@/translations';
 import Translate from '@/components/Translate';
 import { Label } from '@/components/ui/label';
+import Confetti from 'react-confetti';
+import { useToast } from '@/hooks/use-toast';
 
 interface Step3Props {
   nextStep: () => void;
@@ -20,52 +22,181 @@ interface Step3Props {
     image: string;
     icon: React.ReactNode;
   }[];
+  formData?: Record<string, string>;
 }
 
 const Step3GenerateSpeech: React.FC<Step3Props> = ({
   nextStep,
   prevStep,
   selectedSpeechType,
-  speechTypes
+  speechTypes,
+  formData = {}
 }) => {
   const { currentLanguage } = useLanguage();
   const { t } = useTranslation();
+  const [title, setTitle] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+  const { toast } = useToast();
+
+  useEffect(() => {
+    // Set window size for confetti
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const handleGenerate = () => {
+    if (!title.trim()) {
+      toast({
+        title: "Title Required",
+        description: "Please enter a title for your speech",
+        variant: "destructive"
+      });
+      return;
+    }
+
+    setIsGenerating(true);
+    
+    // Simulate speech generation (in a real app, this would call an API)
+    setTimeout(() => {
+      setIsGenerating(false);
+      setShowConfetti(true);
+      
+      toast({
+        title: "Congratulations!",
+        description: "Your speech has been successfully generated!",
+        duration: 5000,
+      });
+      
+      // Automatically proceed to next step after 2.5 seconds
+      setTimeout(() => {
+        nextStep();
+      }, 2500);
+    }, 1500);
+  };
+
+  const selectedType = speechTypes.find(type => type.id === selectedSpeechType) || speechTypes[0];
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle><Translate text="speechLab.generateTitle" /></CardTitle>
-        <CardDescription><Translate text="speechLab.generateDesc" /></CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-6">
-        <div>
-          <Label htmlFor="speechTitle"><Translate text="speechLab.speechTitleLabel" /></Label>
-          <Input 
-            id="speechTitle" 
-            placeholder={t('speechLab.speechTitlePlaceholder', currentLanguage.code)} 
-            className="mt-1"
-          />
-        </div>
-        
-        <div className="p-4 bg-gray-100 rounded-md">
-          <h3 className="font-medium mb-2"><Translate text="common.type" />: {speechTypes.find(type => type.id === selectedSpeechType)?.label || ''}</h3>
-          <Separator className="my-4" />
-          {/* Show summary of speech details here */}
-          <div className="text-sm text-gray-600">
-            <p><Translate text="speechLab.summaryNotice" fallback="Speech details will be used to generate your content" /></p>
+    <>
+      {showConfetti && (
+        <Confetti
+          width={windowSize.width}
+          height={windowSize.height}
+          recycle={false}
+          numberOfPieces={500}
+          tweenDuration={5000}
+        />
+      )}
+      
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2">
+            <Translate text="speechLab.generateTitle" />
+            {showConfetti && <Sparkles className="h-5 w-5 text-yellow-500" />}
+          </CardTitle>
+          <CardDescription><Translate text="speechLab.generateDesc" /></CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-6">
+          <div>
+            <Label htmlFor="speechTitle"><Translate text="speechLab.speechTitleLabel" /></Label>
+            <Input 
+              id="speechTitle" 
+              placeholder={t('speechLab.speechTitlePlaceholder', currentLanguage.code)} 
+              className="mt-1"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
-        </div>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <ButtonCustom onClick={prevStep} variant="outline">
-          <ArrowLeft className="mr-2 h-4 w-4" />
-          <Translate text="speechLab.backButton" />
-        </ButtonCustom>
-        <ButtonCustom onClick={nextStep} variant="magenta">
-          <Translate text="speechLab.generateButton" />
-        </ButtonCustom>
-      </CardFooter>
-    </Card>
+          
+          <div className="p-4 bg-gray-100 rounded-md">
+            <h3 className="font-medium mb-2 flex items-center">
+              <Translate text="common.type" />: {selectedType.label || ''}
+              {selectedType.icon && <span className="ml-2">{selectedType.icon}</span>}
+            </h3>
+            <Separator className="my-4" />
+            <div className="text-sm text-gray-600 space-y-2">
+              <p><Translate text="speechLab.summaryNotice" fallback="Speech details will be used to generate your content" /></p>
+              
+              {/* Summary of questionnaire answers */}
+              {Object.keys(formData).length > 0 && (
+                <div className="mt-4 p-3 bg-white rounded border border-gray-200">
+                  <h4 className="font-medium text-sm mb-2">
+                    <Translate text="speechLab.questionnaireSummary" fallback="Summary of your information:" />
+                  </h4>
+                  <ul className="list-disc pl-5 text-xs space-y-1">
+                    {Object.entries(formData).slice(0, 3).map(([question, answer], idx) => (
+                      <li key={idx}>
+                        <span className="font-medium">{question}:</span> {answer}
+                      </li>
+                    ))}
+                    {Object.keys(formData).length > 3 && (
+                      <li className="italic">
+                        <Translate 
+                          text="speechLab.andMoreDetails" 
+                          fallback="And more details that will be included in your speech..." 
+                        />
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+          
+          {showConfetti && (
+            <div className="bg-purple-100 p-4 rounded-md border border-purple-200 text-center">
+              <PartyPopper className="h-12 w-12 text-purple-600 mx-auto mb-2" />
+              <h3 className="text-lg font-bold text-purple-800 mb-1">
+                <Translate text="speechLab.congratulations" fallback="Congratulations!" />
+              </h3>
+              <p className="text-purple-700">
+                <Translate 
+                  text="speechLab.speechGenerated" 
+                  fallback="Your speech has been successfully generated! Moving to edit screen..." 
+                />
+              </p>
+            </div>
+          )}
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <ButtonCustom onClick={prevStep} variant="outline" disabled={isGenerating || showConfetti}>
+            <ArrowLeft className="mr-2 h-4 w-4" />
+            <Translate text="speechLab.backButton" />
+          </ButtonCustom>
+          
+          {!showConfetti && (
+            <ButtonCustom 
+              onClick={handleGenerate} 
+              variant="magenta"
+              disabled={isGenerating}
+            >
+              {isGenerating ? (
+                <>
+                  <div className="animate-spin mr-2 h-4 w-4 border-2 border-current border-t-transparent rounded-full" />
+                  <Translate text="speechLab.generating" fallback="Generating..." />
+                </>
+              ) : (
+                <Translate text="speechLab.generateButton" />
+              )}
+            </ButtonCustom>
+          )}
+        </CardFooter>
+      </Card>
+    </>
   );
 };
 
