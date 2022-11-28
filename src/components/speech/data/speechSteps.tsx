99bee9589ccd1c3ec68bfe796362f80f049df62b
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2022-11-28 16:18:18 +0600

    Refactor: Split SpeechLabContent into smaller components

---
+++ src/components/speech/data/speechSteps.tsx
@@ -0,0 +1,7 @@
+
+export const speechSteps = [
+  { number: 1, title: 'Select Occasion' },
+  { number: 2, title: 'Let\'s Get Creative' },
+  { number: 3, title: 'Generate Speech' },
+  { number: 4, title: 'Edit & Save' }
+];
