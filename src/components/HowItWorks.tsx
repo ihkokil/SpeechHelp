7738a7424199598ea098ed9d505089c044dec99e
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-01-19 17:12:13 +0600

    Refactor: Restore hero and content

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState, useRef } from 'react';
 import { PencilIcon, SparklesIcon, MicIcon, CheckIcon } from 'lucide-react';
 import { useLanguage } from '@/contexts/LanguageContext';
 import { useTranslation } from '@/translations';
+import Translate from './Translate';
 
 interface StepProps {
   icon: JSX.Element;
@@ -11,9 +12,6 @@ interface StepProps {
 }
 
 const Step = ({ icon, stepNumber, isVisible }: StepProps) => {
-  const { currentLanguage } = useLanguage();
-  const { t } = useTranslation();
-  
   return (
     <div className={`mb-16 relative opacity-0 ${isVisible ? `animate-slide-in stagger-${stepNumber}` : ''}`}>
       <div className="flex items-start">
@@ -27,10 +25,10 @@ const Step = ({ icon, stepNumber, isVisible }: StepProps) => {
         </div>
         <div className="flex-1 pt-1">
           <h3 className="text-xl font-semibold text-pink-600 mb-2">
-            {t(`howItWorks.step${stepNumber}.title`, currentLanguage.code)}
+            <Translate text={`howItWorks.step${stepNumber}.title`} />
           </h3>
           <p className="text-gray-600 mb-6 max-w-md">
-            {t(`howItWorks.step${stepNumber}.description`, currentLanguage.code)}
+            <Translate text={`howItWorks.step${stepNumber}.description`} />
           </p>
         </div>
       </div>
@@ -41,8 +39,6 @@ const Step = ({ icon, stepNumber, isVisible }: StepProps) => {
 const HowItWorks = () => {
   const [isVisible, setIsVisible] = useState(false);
   const sectionRef = useRef<HTMLDivElement>(null);
-  const { currentLanguage } = useLanguage();
-  const { t } = useTranslation();
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -77,10 +73,10 @@ const HowItWorks = () => {
       <div className="container mx-auto px-6 md:px-12">
         <div className="text-center max-w-3xl mx-auto mb-16" ref={sectionRef}>
           <h2 className={`text-3xl md:text-4xl font-bold mb-6 opacity-0 ${isVisible ? 'animate-fade-in' : ''}`}>
-            {t('howItWorks.title', currentLanguage.code)} <span className="text-pink-600">Works</span>
+            <Translate text="howItWorks.title" /> <span className="text-pink-600">Works</span>
           </h2>
           <p className={`text-lg text-gray-600 opacity-0 ${isVisible ? 'animate-fade-in stagger-1' : ''}`}>
-            {t('howItWorks.subtitle', currentLanguage.code)}
+            <Translate text="howItWorks.subtitle" />
           </p>
         </div>
 
