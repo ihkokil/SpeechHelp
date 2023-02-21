90e675d2fc340983183fd7c68af0854f828efa3e
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-02-21 08:47:15 +0600

    Reverted to edit edt-d77bb272-2fcc-4a0f-90a9-6d4006010727: "Fix: Hero video playback issues"

--- src/components/speech/utils/speechUtils.ts
+++
@@ -1,7 +0,0 @@
-
-import { speechQuestionnaires, QuestionnaireItem } from '../data/speechQuestionnaires';
-
-// Get the questionnaire for a specific speech type, defaulting to "other" if not found
-export const getQuestionnaire = (selectedSpeechType: string): QuestionnaireItem[] => {
-  return speechQuestionnaires[selectedSpeechType] || speechQuestionnaires.other;
-};
