5c5bbb379a69b4352aebe1da6631c509a5b55f50
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-03-27 12:19:08 +0600

    Refactor: Remove confetti and change bubble shape

--- src/components/speech/components/EncouragementMessage.tsx
+++ src/components/speech/components/EncouragementMessage.tsx
@@ -1,7 +1,6 @@
 
 import React, { useEffect, useState, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import confetti from 'react-confetti';
 
 interface EncouragementMessageProps {
   currentQuestionIndex: number;
@@ -14,7 +13,6 @@ const EncouragementMessage: React.FC<EncouragementMessageProps> = ({
 }) => {
   const [message, setMessage] = useState<string>('');
   const [showMessage, setShowMessage] = useState(false);
-  const [showConfetti, setShowConfetti] = useState(false);
   const [lastShownIndex, setLastShownIndex] = useState(-1);
   const [usedMessageIndices, setUsedMessageIndices] = useState<number[]>([]);
   const [appearanceCount, setAppearanceCount] = useState(0);
@@ -93,22 +91,16 @@ const EncouragementMessage: React.FC<EncouragementMessageProps> = ({
       // Increment appearance count
       setAppearanceCount(prev => prev + 1);
       
-      // Show the message and confetti
+      // Show the message
       setShowMessage(true);
-      setShowConfetti(true);
       
-      // Hide the message and confetti after delays
+      // Hide the message after delay
       const messageTimer = setTimeout(() => {
         setShowMessage(false);
       }, 5000); // Show message for 5 seconds
       
-      const confettiTimer = setTimeout(() => {
-        setShowConfetti(false);
-      }, 2000); // Show confetti for 2 seconds
-      
       return () => {
         clearTimeout(messageTimer);
-        clearTimeout(confettiTimer);
       };
     }
   }, [currentQuestionIndex, totalQuestions, encouragingMessages, lastShownIndex, usedMessageIndices, appearanceCount]);
@@ -128,43 +120,32 @@ const EncouragementMessage: React.FC<EncouragementMessageProps> = ({
             transition={{ duration: 0.5 }}
             className="relative w-full max-w-md"
           >
-            {/* Round thought bubble */}
-            <div className="relative bg-white p-5 rounded-full shadow-lg">
-              {/* Top bubble circles */}
-              <div className="absolute -top-5 left-1/4 w-8 h-8 bg-white rounded-full shadow-sm"></div>
-              <div className="absolute -top-7 left-1/3 w-10 h-10 bg-white rounded-full shadow-sm"></div>
-              <div className="absolute -top-4 left-1/2 w-6 h-6 bg-white rounded-full shadow-sm"></div>
-              
-              {/* Thought bubble tail - small circles getting smaller */}
-              <div className="absolute -bottom-8 left-1/4 w-5 h-5 bg-white rounded-full shadow-sm"></div>
-              <div className="absolute -bottom-12 left-1/5 w-4 h-4 bg-white rounded-full shadow-sm"></div>
-              <div className="absolute -bottom-16 left-[12%] w-3 h-3 bg-white rounded-full shadow-sm"></div>
-              
-              {/* Message content with gradient background */}
-              <div className="bg-gradient-to-r from-blue-50 to-purple-50 p-4 rounded-full border border-purple-100 text-center">
-                <p className="text-purple-800 font-medium">{message}</p>
+            {/* Cloud/Puffy thought bubble */}
+            <div className="relative">
+              {/* Main cloud shape */}
+              <div className="bg-white p-6 rounded-[50px] shadow-lg relative">
+                {/* Cloud bumps/puffs on top */}
+                <div className="absolute -top-10 left-1/4 w-20 h-20 bg-white rounded-full shadow-sm"></div>
+                <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 w-28 h-24 bg-white rounded-full shadow-sm"></div>
+                <div className="absolute -top-7 right-1/4 w-16 h-16 bg-white rounded-full shadow-sm"></div>
+                
+                {/* Cloud bumps/puffs on sides */}
+                <div className="absolute top-1/4 -left-8 w-16 h-16 bg-white rounded-full shadow-sm"></div>
+                <div className="absolute top-1/2 -left-6 w-12 h-12 bg-white rounded-full shadow-sm"></div>
+                <div className="absolute top-1/4 -right-8 w-16 h-16 bg-white rounded-full shadow-sm"></div>
+                <div className="absolute top-1/2 -right-6 w-12 h-12 bg-white rounded-full shadow-sm"></div>
+                
+                {/* Bottom cloud puffs */}
+                <div className="absolute -bottom-6 left-1/4 w-14 h-14 bg-white rounded-full shadow-sm"></div>
+                <div className="absolute -bottom-8 left-1/2 transform -translate-x-1/2 w-16 h-16 bg-white rounded-full shadow-sm"></div>
+                <div className="absolute -bottom-6 right-1/4 w-14 h-14 bg-white rounded-full shadow-sm"></div>
+                
+                {/* Message content with gradient background */}
+                <div className="bg-gradient-to-r from-blue-50 to-purple-50 p-6 rounded-[40px] border border-purple-100 text-center relative z-10">
+                  <p className="text-purple-800 font-medium">{message}</p>
+                </div>
               </div>
             </div>
-            
-            {/* Pink and purple confetti positioned over the thought bubble */}
-            {showConfetti && bubbleRef.current && (
-              <div className="absolute top-0 left-0 right-0 pointer-events-none z-40">
-                {React.createElement(confetti, {
-                  width: bubbleRef.current.offsetWidth,
-                  height: bubbleRef.current.offsetHeight,
-                  recycle: false,
-                  numberOfPieces: 50,
-                  gravity: 0.2,
-                  colors: ['#FFC0CB', '#FF69B4', '#DA70D6', '#BA55D3', '#9370DB', '#8A2BE2'], // Pink and purple shades
-                  confettiSource: {
-                    x: bubbleRef.current.offsetWidth / 2,
-                    y: bubbleRef.current.offsetHeight / 2,
-                    w: 0,
-                    h: 0
-                  }
-                })}
-              </div>
-            )}
           </motion.div>
         )}
       </AnimatePresence>
