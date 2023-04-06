c6ef5a7de5ee2f614c34bf48c481c1017c44f28c
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-04-06 09:29:35 +0600

    feat: Enhance encouragement message animation

--- src/components/speech/components/EncouragementMessage.tsx
+++ src/components/speech/components/EncouragementMessage.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import { Star } from 'lucide-react';
+import { Star, CircleDot } from 'lucide-react';
 
 interface EncouragementMessageProps {
   currentQuestionIndex: number;
@@ -13,6 +13,7 @@ const EncouragementMessage: React.FC<EncouragementMessageProps> = ({
   totalQuestions 
 }) => {
   const [message, setMessage] = useState<string>('');
+  const [isVisible, setIsVisible] = useState(false);
 
   const encouragingMessages = [
     "Your insights are the secret ingredient to a brilliant speech!",
@@ -31,36 +32,79 @@ const EncouragementMessage: React.FC<EncouragementMessageProps> = ({
     // Pick a message based on current question index to ensure consistency
     const messageIndex = currentQuestionIndex % encouragingMessages.length;
     setMessage(encouragingMessages[messageIndex]);
+    
+    // Trigger animation sequence
+    setIsVisible(true);
+    
+    return () => {
+      setIsVisible(false);
+    };
   }, [currentQuestionIndex]);
 
-  return (
-    <motion.div
-      key={`encouragement-${currentQuestionIndex}`}
-      initial={{ opacity: 0, scale: 0.8, y: 10 }}
-      animate={{ 
-        opacity: 1, 
-        scale: 1, 
-        y: 0
-      }}
-      transition={{ 
-        duration: 0.4,
+  // Animation variants
+  const circleVariants = {
+    hidden: { 
+      scale: 0.1,
+      opacity: 0,
+      rotate: 0
+    },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      rotate: 360,
+      transition: {
+        duration: 0.8,
         type: "spring",
-        stiffness: 150,
-        damping: 20
-      }}
-      className="w-full max-w-md mx-auto"
-    >
-      {/* Message bubble */}
+        stiffness: 100,
+        damping: 15,
+        rotate: {
+          duration: 1,
+          ease: "easeInOut",
+          delay: 0.3
+        }
+      }
+    }
+  };
+
+  const contentVariants = {
+    hidden: { 
+      opacity: 0,
+      scale: 0.6
+    },
+    visible: { 
+      opacity: 1,
+      scale: 1,
+      transition: { 
+        delay: 0.7,
+        duration: 0.5 
+      }
+    }
+  };
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      {/* Message bubble with enhanced animation */}
       <div className="flex items-center justify-center">
-        <div className="w-52 h-52 bg-gradient-to-br from-purple-100 to-indigo-50 rounded-full shadow-lg flex items-center justify-center p-4 border border-purple-200">
-          <div className="text-center">
+        <motion.div
+          key={`encouragement-${currentQuestionIndex}`}
+          variants={circleVariants}
+          initial="hidden"
+          animate={isVisible ? "visible" : "hidden"}
+          className="w-52 h-52 bg-gradient-to-br from-purple-100 to-indigo-50 rounded-full shadow-lg flex items-center justify-center p-4 border border-purple-200 overflow-hidden"
+        >
+          <motion.div 
+            className="text-center"
+            variants={contentVariants}
+            initial="hidden"
+            animate={isVisible ? "visible" : "hidden"}
+          >
             <Star className="h-6 w-6 text-purple-600 mb-1 mx-auto" fill="#E5DEFF" strokeWidth={2} />
             <h4 className="font-bold text-purple-900 mb-2 uppercase text-sm">Speech Writing Tip</h4>
             <p className="text-purple-800 font-medium text-sm">{message}</p>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
