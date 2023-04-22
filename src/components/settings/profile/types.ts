2dc3ca1acd0e240369b76dfa25b18eb1d1b66b85
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-04-22 23:32:43 +0600

    Refactor: Break down ProfileSettings component

---
+++ src/components/settings/profile/types.ts
@@ -0,0 +1,17 @@
+
+import * as z from 'zod';
+
+export const profileFormSchema = z.object({
+  firstName: z.string().min(2, { message: 'First name must be at least 2 characters.' }),
+  lastName: z.string().min(2, { message: 'Last name must be at least 2 characters.' }),
+  email: z.string().email({ message: 'Please enter a valid email address.' }),
+  phone: z.string().optional(),
+  countryCode: z.string().default('US'),
+  streetAddress: z.string().optional(),
+  city: z.string().optional(),
+  state: z.string().optional(),
+  zipCode: z.string().optional(),
+  country: z.string().optional(),
+});
+
+export type ProfileFormValues = z.infer<typeof profileFormSchema>;
