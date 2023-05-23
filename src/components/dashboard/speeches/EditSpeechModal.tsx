66d03cbd61ff81e116dc51e89a6530af4100d9d6
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-05-23 19:58:12 +0600

    Add download and print functionality

--- src/components/dashboard/speeches/EditSpeechModal.tsx
+++ src/components/dashboard/speeches/EditSpeechModal.tsx
@@ -12,6 +12,9 @@ import { Input } from '@/components/ui/input';
 import { ButtonCustom } from '@/components/ui/button-custom';
 import Translate from '@/components/Translate';
 import SpeechContentEditor from '@/components/speech/components/SpeechContentEditor';
+import { Download, Printer } from 'lucide-react';
+import { useToast } from '@/hooks/use-toast';
+import { getSpeechTypeLabel } from './speech-utils';
 
 interface EditSpeechModalProps {
   isOpen: boolean;
@@ -34,10 +37,110 @@ const EditSpeechModal = ({
   setEditContent, 
   onSave 
 }: EditSpeechModalProps) => {
+  const { toast } = useToast();
+  
   if (!speech) return null;
 
   const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setEditContent(e.target.value);
+  };
+
+  // Extract the proper content from speech
+  const extractContentForExport = (content: string): string => {
+    if (content.includes('{"content"')) {
+      try {
+        const jsonContent = JSON.parse(content);
+        return jsonContent.content || content;
+      } catch (e) {
+        return content;
+      }
+    }
+    return content;
+  };
+
+  // Download speech as a text file
+  const handleDownload = () => {
+    const processedContent = extractContentForExport(editContent);
+    const blob = new Blob([processedContent], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${editTitle.trim() || 'speech'}.txt`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+
+    toast({
+      title: "Download Started",
+      description: "Your speech is being downloaded as a text file.",
+    });
+  };
+
+  // Print the speech content
+  const handlePrint = () => {
+    const processedContent = extractContentForExport(editContent);
+    const title = editTitle;
+    const type = getSpeechTypeLabel(speech.speech_type);
+    const printWindow = window.open('', '_blank');
+    
+    if (printWindow) {
+      // Create a nicely formatted HTML page for printing
+      printWindow.document.write(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>${title} - Print</title>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              margin: 1.5rem;
+              line-height: 1.6;
+            }
+            .title {
+              font-size: 24px;
+              font-weight: bold;
+              margin-bottom: 4px;
+              color: #6b21a8;
+            }
+            .type {
+              font-size: 14px;
+              color: #666;
+              margin-bottom: 16px;
+            }
+            .content {
+              white-space: pre-wrap;
+              font-size: 14px;
+            }
+            @media print {
+              body {
+                margin: 1cm;
+              }
+            }
+          </style>
+        </head>
+        <body>
+          <div class="title">${title}</div>
+          <div class="type">${type}</div>
+          <hr />
+          <div class="content">${processedContent.replace(/\n/g, '<br>')}</div>
+        </body>
+        </html>
+      `);
+      
+      printWindow.document.close();
+      
+      // Trigger print dialog
+      printWindow.setTimeout(() => {
+        printWindow.print();
+      }, 500);
+    } else {
+      toast({
+        title: "Error",
+        description: "Unable to open print window. Please check your popup settings.",
+        variant: "destructive"
+      });
+    }
   };
 
   return (
@@ -66,6 +169,24 @@ const EditSpeechModal = ({
               content={editContent}
               onContentChange={handleContentChange}
             />
+          </div>
+          <div className="flex flex-wrap gap-2">
+            <ButtonCustom 
+              variant="outline" 
+              size="sm"
+              onClick={handleDownload}
+            >
+              <Download className="h-4 w-4 mr-2" />
+              <Translate text="common.download" fallback="Download" />
+            </ButtonCustom>
+            <ButtonCustom 
+              variant="outline" 
+              size="sm"
+              onClick={handlePrint}
+            >
+              <Printer className="h-4 w-4 mr-2" />
+              <Translate text="common.print" fallback="Print" />
+            </ButtonCustom>
           </div>
         </div>
         <DialogFooter className="mt-4">
