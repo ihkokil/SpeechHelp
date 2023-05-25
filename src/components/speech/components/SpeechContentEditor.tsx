17a4f8061692cdd541cfe5dedff847980166afeb
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-05-25 22:13:58 +0600

    Fix: Enable HTML editing in edit speech modal

--- src/components/speech/components/SpeechContentEditor.tsx
+++ src/components/speech/components/SpeechContentEditor.tsx
@@ -18,8 +18,9 @@ const SpeechContentEditor: React.FC<SpeechContentEditorProps> = ({
 }) => {
   const [viewMode, setViewMode] = useState<'edit' | 'preview'>('edit');
   const [processedContent, setProcessedContent] = useState(content);
+  const [htmlContent, setHtmlContent] = useState('');
 
-  // Process JSON content if needed
+  // Process content on initial load and when content changes
   useEffect(() => {
     // Try to parse JSON if the content appears to be JSON
     if (content.includes('{"content"')) {
@@ -33,11 +34,73 @@ const SpeechContentEditor: React.FC<SpeechContentEditorProps> = ({
     } else {
       setProcessedContent(content);
     }
+
+    // Generate HTML representation for the editor when in preview mode
+    const formattedHtml = formatSpeechContent(content);
+    setHtmlContent(formattedHtml);
   }, [content]);
+
+  // Format content for preview - same function as in SpeechPreview
+  const formatSpeechContent = (text: string): string => {
+    if (!text) return '';
+    
+    let formattedText = text;
+    
+    // Remove the raw JSON if it appears in the content
+    if (formattedText.includes('{"content"')) {
+      try {
+        const jsonContent = JSON.parse(formattedText);
+        formattedText = jsonContent.content || formattedText;
+      } catch (e) {
+        console.log('Failed to parse JSON content');
+      }
+    }
+    
+    // Handle headings with improved styling
+    formattedText = formattedText.replace(/^# (.+)$/gm, '<h1 class="text-2xl font-bold mb-4 text-purple-800">$1</h1>');
+    formattedText = formattedText.replace(/^## (.+)$/gm, '<h2 class="text-xl font-bold mt-6 mb-3 text-purple-700">$1</h2>');
+    formattedText = formattedText.replace(/^### (.+)$/gm, '<h3 class="text-lg font-bold mt-5 mb-2 text-purple-600">$1</h3>');
+    
+    // Handle bold text
+    formattedText = formattedText.replace(/\*\*(.+?)\*\*/g, '<strong class="font-bold">$1</strong>');
+    
+    // Handle italic text
+    formattedText = formattedText.replace(/\*(.+?)\*/g, '<em class="italic">$1</em>');
+    
+    // Handle horizontal rule with a more prominent styling
+    formattedText = formattedText.replace(/^---$/gm, '<hr class="my-6 border-t-2 border-purple-300" />');
+    
+    // Add spacing between paragraphs
+    formattedText = formattedText.replace(/\n\n/g, '</p><p class="mb-4">');
+    
+    // Handle "Your Speech Inputs" section
+    if (formattedText.includes('Your Speech Inputs')) {
+      formattedText = formattedText.replace(
+        /(Your Speech Inputs.*?)---/s, 
+        '<div class="bg-purple-50 p-4 rounded-md mb-6 border border-purple-200">$1</div>'
+      );
+    }
+    
+    // Make question-answer pairs in the input section more readable
+    formattedText = formattedText.replace(
+      /<strong class="font-bold">(.+?)<\/strong> (.+?)(?=<\/p>|<strong|$)/g, 
+      '<div class="mb-2"><span class="font-medium text-purple-700">$1:</span> <span class="text-gray-800">$2</span></div>'
+    );
+    
+    // Wrap the content in a paragraph tag with proper spacing
+    formattedText = `<p class="mb-4">${formattedText}</p>`;
+    
+    // Fix any double wrapping of paragraph tags
+    formattedText = formattedText.replace(/<p class="mb-4"><p class="mb-4">/g, '<p class="mb-4">');
+    formattedText = formattedText.replace(/<\/p><\/p>/g, '</p>');
+    
+    return formattedText;
+  };
 
   // Custom handler for content changes to maintain JSON structure if it exists
   const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const newValue = e.target.value;
+    setProcessedContent(newValue);
     
     // If original content was JSON, maintain that structure
     if (content.includes('{"content"')) {
