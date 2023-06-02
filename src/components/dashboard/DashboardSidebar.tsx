3b28af1da470db303a293dedfba000cca9d7bd46
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-06-02 17:09:55 +0600

    Fix: Resolve missing logo images

--- src/components/dashboard/DashboardSidebar.tsx
+++ src/components/dashboard/DashboardSidebar.tsx
@@ -1,3 +1,4 @@
+
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '@/contexts/AuthContext';
 import { cn } from '@/lib/utils';
@@ -25,8 +26,8 @@ const DashboardSidebar = () => {
   const navigate = useNavigate();
   const { signOut, user } = useAuth();
   
-  // Using a relative path that works in both development and production
-  const logoPath = "/Speech Help - Logo.svg";
+  // Updated to use the correct logo path
+  const logoPath = "/speech-help-new-logo.svg";
   
   // Get current path for highlighting active item
   const currentPath = window.location.pathname;
