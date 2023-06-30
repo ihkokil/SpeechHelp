401993123917df036796a4d56409f4a6e470b1bd
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-06-30 11:38:44 +0600

    Fix: Improve tab order for form fields

--- src/components/settings/profile/components/StreetAddressField.tsx
+++ src/components/settings/profile/components/StreetAddressField.tsx
@@ -1,9 +1,9 @@
 
 import { UseFormReturn } from 'react-hook-form';
 import { FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form';
-import { Textarea } from '@/components/ui/textarea';
+import { Input } from '@/components/ui/input';
 import { ProfileFormValues } from '../types';
-import { Home } from 'lucide-react';
+import { MapPin } from 'lucide-react';
 
 interface StreetAddressFieldProps {
   form: UseFormReturn<ProfileFormValues>;
@@ -19,10 +19,15 @@ const StreetAddressField = ({ form }: StreetAddressFieldProps) => {
           <FormLabel>Street Address</FormLabel>
           <FormControl>
             <div className="relative">
-              <div className="absolute left-3 top-3">
-                <Home className="h-4 w-4 text-gray-500" />
+              <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+                <MapPin className="h-4 w-4 text-gray-500" />
               </div>
-              <Textarea placeholder="123 Main St, Apt 4B" {...field} tabIndex={7} className="w-full pl-10" />
+              <Input 
+                {...field} 
+                placeholder="Street Address"
+                className="pl-10"
+                tabIndex={4}
+              />
             </div>
           </FormControl>
           <FormMessage />
