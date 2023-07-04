f722cdabbe180c66c83a652426d86ec6433892e4
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-07-04 14:29:27 +0600

    Refactor: Improve AI prompt generation

--- src/components/speech/components/SpeechGenerationProgress.tsx
+++ src/components/speech/components/SpeechGenerationProgress.tsx
@@ -1,31 +1,53 @@
 
 import React from 'react';
-import { PartyPopper } from 'lucide-react';
+import { Progress } from '@/components/ui/progress';
+import { useEffect, useState } from 'react';
 import Confetti from 'react-confetti';
 
 interface SpeechGenerationProgressProps {
-  visible: boolean;
+  showConfetti: boolean;
 }
 
-const SpeechGenerationProgress: React.FC<SpeechGenerationProgressProps> = ({ visible }) => {
-  if (!visible) return null;
+const SpeechGenerationProgress: React.FC<SpeechGenerationProgressProps> = ({ showConfetti }) => {
+  const [progress, setProgress] = useState(0);
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress(prevProgress => {
+        if (prevProgress >= 100) {
+          clearInterval(interval);
+          return 100;
+        }
+        return prevProgress + 1;
+      });
+    }, 50);
+    
+    return () => clearInterval(interval);
+  }, []);
   
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <Confetti
-        width={window.innerWidth}
-        height={window.innerHeight}
-        recycle={false}
-        numberOfPieces={500}
-      />
-      <div className="rounded-full bg-white/90 backdrop-blur-sm p-10 shadow-lg z-10 text-center w-80 h-80 flex flex-col items-center justify-center border-4 border-pink-500 animate-scale-in">
-        <div className="mb-4">
-          <PartyPopper className="h-16 w-16 text-pink-600 mb-2" />
-        </div>
-        <h2 className="text-3xl font-bold text-pink-600 mb-2">Congratulations</h2>
-        <h3 className="text-2xl font-bold text-pink-600 mb-4">You Did It!</h3>
-        <p className="text-gray-700">Your speech is being generated...</p>
+    <div className="space-y-4">
+      <div className="text-center font-medium">
+        <p className="mb-2">Crafting your speech...</p>
+        <p className="text-sm text-muted-foreground">
+          {progress < 30 && "Gathering your inputs..."}
+          {progress >= 30 && progress < 60 && "Analyzing speech structure..."}
+          {progress >= 60 && progress < 85 && "Polishing language and tone..."}
+          {progress >= 85 && progress < 100 && "Finalizing your speech..."}
+          {progress >= 100 && "Speech ready!"}
+        </p>
       </div>
+      
+      <Progress value={progress} className="h-2" />
+      
+      {showConfetti && progress >= 100 && (
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          recycle={false}
+          numberOfPieces={200}
+        />
+      )}
     </div>
   );
 };
