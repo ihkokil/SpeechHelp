ec025f03cac9265b65338308712a11b020d431dc
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-07-24 21:01:42 +0600

    Fix: Widen speech type badges

--- src/components/dashboard/speeches/SpeechesTable.tsx
+++ src/components/dashboard/speeches/SpeechesTable.tsx
@@ -44,7 +44,7 @@ const SpeechesTable = ({ speeches, onView, onEdit, onDelete }: SpeechesTableProp
             <TableCell className="font-medium">{speech.title}</TableCell>
             <TableCell className="text-center">
               <Badge 
-                className={`${getTypeColor(speech.speech_type)} mx-auto inline-flex justify-center w-24 h-6 px-2 whitespace-nowrap overflow-hidden text-ellipsis`}
+                className={`${getTypeColor(speech.speech_type)} mx-auto inline-flex justify-center w-32 h-6 px-2 whitespace-nowrap overflow-hidden text-ellipsis`}
                 title={getSpeechTypeLabel(speech.speech_type)}
               >
                 {getSpeechTypeLabel(speech.speech_type)}
