297678ac4d0efd1e3315779240423790571487fc
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-08-01 21:42:42 +0600

    Fix: Dashboard layout and speech restoration

--- src/services/speechService.ts
+++ src/services/speechService.ts
@@ -9,22 +9,36 @@ export const useSpeechService = () => {
   const fetchSpeeches = async (userId: string | undefined) => {
     if (!userId) return [];
     
-    const { data, error } = await supabase
-      .from('speeches')
-      .select('*')
-      .order('created_at', { ascending: false });
-    
-    if (error) {
-      console.error('Error fetching speeches:', error);
+    try {
+      console.log('Fetching speeches for user:', userId);
+      
+      const { data, error } = await supabase
+        .from('speeches')
+        .select('*')
+        .eq('user_id', userId)
+        .order('created_at', { ascending: false });
+      
+      if (error) {
+        console.error('Error fetching speeches:', error);
+        toast({
+          title: "Error fetching speeches",
+          description: error.message,
+          variant: "destructive"
+        });
+        return [];
+      }
+      
+      console.log('Fetched speeches:', data);
+      return data as Speech[] || [];
+    } catch (error: any) {
+      console.error('Exception fetching speeches:', error);
       toast({
         title: "Error fetching speeches",
-        description: error.message,
+        description: error.message || "Failed to fetch speeches",
         variant: "destructive"
       });
       return [];
     }
-    
-    return data as Speech[] || [];
   };
 
   const saveSpeech = async (userId: string, title: string, content: string, speechType: string) => {
