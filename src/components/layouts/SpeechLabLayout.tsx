cda723efd537a2e1bd4c017b553456536e1d7873
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-08-03 19:40:27 +0600

    Fix: Improve dashboard responsiveness

--- src/components/layouts/SpeechLabLayout.tsx
+++ src/components/layouts/SpeechLabLayout.tsx
@@ -1,20 +1,32 @@
 
 import React from 'react';
 import DashboardSidebar from '@/components/dashboard/DashboardSidebar';
+import MobileNav from '@/components/navigation/MobileNav';
+import { useIsMobile } from '@/hooks/use-mobile';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 interface SpeechLabLayoutProps {
   children: React.ReactNode;
 }
 
 const SpeechLabLayout: React.FC<SpeechLabLayoutProps> = ({ children }) => {
+  const isMobile = useIsMobile();
+
   return (
-    <div className="min-h-screen flex">
-      {/* Dashboard Sidebar */}
-      <DashboardSidebar />
+    <div className="min-h-screen flex flex-col">
+      {/* Show Mobile Navigation on mobile devices */}
+      {isMobile && <MobileNav />}
       
-      {/* Main Content */}
-      <div className="flex-1 bg-gray-50 overflow-auto">
-        {children}
+      <div className="flex flex-1 overflow-hidden">
+        {/* Show Dashboard Sidebar on non-mobile devices */}
+        {!isMobile && <DashboardSidebar />}
+        
+        {/* Main Content - with ScrollArea to prevent horizontal scrolling */}
+        <ScrollArea className="flex-1 h-screen">
+          <div className="bg-gray-50 p-4">
+            {children}
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
--- src/components/navigation/MobileNav.tsx
+++ src/components/navigation/MobileNav.tsx
@@ -4,6 +4,7 @@ import { Menu, X } from "lucide-react";
 import NavLinks from "./NavLinks";
 import UserMenu from "../UserMenu";
 import LanguageSelector from "../common/LanguageSelector";
+import { Link } from "react-router-dom";
 
 const MobileNav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,26 +12,36 @@ const MobileNav = () => {
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
   const closeMenu = () => setIsMenuOpen(false);
 
+  // Using Supabase hosted SVG file for logo
+  const logoPath = "https://yotrueuqjxmgcwlbbyps.supabase.co/storage/v1/object/public/svg_files//Speech%20Help%20Logo.svg";
+
   return (
-    <div className="md:hidden">
-      <div className="flex items-center space-x-4">
-        <UserMenu />
-        <LanguageSelector />
-        <button
-          onClick={toggleMenu}
-          className="text-gray-700"
-        >
-          {isMenuOpen ? (
-            <X className="h-6 w-6" />
-          ) : (
-            <Menu className="h-6 w-6" />
-          )}
-        </button>
+    <div className="md:hidden w-full">
+      <div className="flex items-center justify-between p-4">
+        <Link to="/" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+          <img src={logoPath} alt="Speech Help" className="h-8" />
+        </Link>
+        
+        <div className="flex items-center space-x-4">
+          <UserMenu />
+          <LanguageSelector />
+          <button
+            onClick={toggleMenu}
+            className="text-gray-700"
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          >
+            {isMenuOpen ? (
+              <X className="h-6 w-6" />
+            ) : (
+              <Menu className="h-6 w-6" />
+            )}
+          </button>
+        </div>
       </div>
 
       {/* Mobile Navigation Menu */}
       {isMenuOpen && (
-        <div className="mt-4 bg-white rounded-lg shadow-lg p-4">
+        <div className="bg-white shadow-lg p-4 max-h-[calc(100vh-4rem)] overflow-y-auto">
           <div className="flex flex-col space-y-4">
             <NavLinks isMobile onItemClick={closeMenu} />
           </div>
