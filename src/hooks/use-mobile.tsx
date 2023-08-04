b03e69ca7107f1b20446f97f015c89393ec78a2b
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-08-04 22:08:22 +0600

    Reverted to edit edt-c9c2fe1a-d990-4f57-98c3-71b032364e8e: "Fix: Update updated_at timestamp

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,63 +1,19 @@
-
 import * as React from "react"
 
-// Defining breakpoints for better responsive control
-export const BREAKPOINTS = {
-  MOBILE: 640,  // sm
-  TABLET: 768,  // md
-  LAPTOP: 1024, // lg
-  DESKTOP: 1280 // xl
-}
+const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean>(false)
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth < BREAKPOINTS.TABLET)
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-
-    // Check on mount
-    checkMobile()
-
-    // Add event listener for window resize
-    window.addEventListener("resize", checkMobile)
-    
-    // Clean up
-    return () => window.removeEventListener("resize", checkMobile)
+    mql.addEventListener("change", onChange)
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return isMobile
-}
-
-export function useResponsive() {
-  const [screenSize, setScreenSize] = React.useState({
-    isMobile: false,
-    isTablet: false,
-    isLaptop: false,
-    isDesktop: false
-  })
-
-  React.useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth
-      setScreenSize({
-        isMobile: width < BREAKPOINTS.MOBILE,
-        isTablet: width >= BREAKPOINTS.MOBILE && width < BREAKPOINTS.LAPTOP,
-        isLaptop: width >= BREAKPOINTS.LAPTOP && width < BREAKPOINTS.DESKTOP,
-        isDesktop: width >= BREAKPOINTS.DESKTOP
-      })
-    }
-
-    // Check on mount
-    handleResize()
-
-    // Add event listener for window resize
-    window.addEventListener("resize", handleResize)
-    
-    // Clean up
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
-
-  return screenSize
+  return !!isMobile
 }
