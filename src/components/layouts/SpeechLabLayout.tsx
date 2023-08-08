3b9cd60f5f6281b10758250274832d9195b8d427
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-08-08 10:46:34 +0600

    Fix: Improve Contact Sales button and link

--- src/components/layouts/SpeechLabLayout.tsx
+++ src/components/layouts/SpeechLabLayout.tsx
@@ -1,19 +1,22 @@
 
 import React from 'react';
 import DashboardSidebar from '@/components/dashboard/DashboardSidebar';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 interface SpeechLabLayoutProps {
   children: React.ReactNode;
 }
 
 const SpeechLabLayout: React.FC<SpeechLabLayoutProps> = ({ children }) => {
+  const isMobile = useIsMobile();
+  
   return (
-    <div className="min-h-screen flex">
+    <div className="min-h-screen flex flex-col md:flex-row">
       {/* Dashboard Sidebar */}
       <DashboardSidebar />
       
       {/* Main Content */}
-      <div className="flex-1 bg-gray-50 overflow-auto">
+      <div className={`flex-1 bg-gray-50 ${isMobile ? 'w-full' : 'overflow-auto'}`}>
         {children}
       </div>
     </div>
