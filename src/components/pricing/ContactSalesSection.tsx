3370e118e3398bb7288739c97ef50005d14b5d15
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-08-09 09:01:58 +0600

    Fix: Contact form scroll issue

--- src/components/pricing/ContactSalesSection.tsx
+++ src/components/pricing/ContactSalesSection.tsx
@@ -1,9 +1,16 @@
 
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const ContactSalesSection: React.FC = () => {
+  const navigate = useNavigate();
+  
+  const handleContactClick = () => {
+    // Navigate to the home page with the contact hash
+    navigate('/#contact');
+  };
+
   return (
     <div className="mt-16 max-w-3xl mx-auto text-center">
       <h2 className="text-2xl font-bold mb-4">Need a Custom Solution?</h2>
@@ -11,14 +18,13 @@ const ContactSalesSection: React.FC = () => {
         We offer custom enterprise plans for teams and organizations.
         Contact us to discuss your specific needs.
       </p>
-      <Link to="/#contact">
-        <Button 
-          variant="outline" 
-          className="border-purple-600 text-purple-600 hover:bg-purple-600 hover:text-white transition-colors"
-        >
-          Contact Sales
-        </Button>
-      </Link>
+      <Button 
+        variant="outline" 
+        className="border-purple-600 text-purple-600 hover:bg-purple-600 hover:text-white transition-colors"
+        onClick={handleContactClick}
+      >
+        Contact Sales
+      </Button>
     </div>
   );
 };
