9b5afb54a81bd9ab1f50d400a0f71d1d633e7456
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-08-14 23:15:05 +0600

    Refactor: Implement address auto-population

--- src/components/settings/profile/components/CountrySelector.tsx
+++ src/components/settings/profile/components/CountrySelector.tsx
@@ -34,7 +34,7 @@ const CountrySelector = ({ form, handleCountryChange }: CountrySelectorProps) =>
                 defaultValue={field.value || ''}
               >
                 <SelectTrigger 
-                  className="w-full pl-10" 
+                  className="w-full pl-10 focus:border-pink-500" 
                   data-focus-visible="true"
                 >
                   <SelectValue placeholder="Select Country of Residence" />
