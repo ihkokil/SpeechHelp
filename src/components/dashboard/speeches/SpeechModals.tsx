74c6b42c2c3a9e302a31a0e5f2286d6fa23d6e4a
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-11-09 15:48:09 +0600

    Add upcoming speech reminder feature

--- src/components/dashboard/speeches/SpeechModals.tsx
+++ src/components/dashboard/speeches/SpeechModals.tsx
@@ -1,69 +1,111 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Speech } from '@/types/auth';
 import ViewSpeechModal from './ViewSpeechModal';
 import EditSpeechModal from './EditSpeechModal';
 import DeleteSpeechAlert from './DeleteSpeechAlert';
+import { useAuth } from '@/contexts/AuthContext';
+import { toast } from 'sonner';
 
-interface SpeechModalsProps {
+export interface SpeechModalsProps {
   selectedSpeech: Speech | null;
   isViewModalOpen: boolean;
+  setIsViewModalOpen: (open: boolean) => void;
   isEditModalOpen: boolean;
+  setIsEditModalOpen: (open: boolean) => void;
   isDeleteAlertOpen: boolean;
-  setIsViewModalOpen: (isOpen: boolean) => void;
-  setIsEditModalOpen: (isOpen: boolean) => void;
-  setIsDeleteAlertOpen: (isOpen: boolean) => void;
-  editTitle: string;
-  editContent: string;
-  setEditTitle: (title: string) => void;
-  setEditContent: (content: string) => void;
-  onSaveEdit: () => void;
-  onConfirmDelete: () => void;
+  setIsDeleteAlertOpen: (open: boolean) => void;
   onEditClick: (speech: Speech) => void;
 }
 
 const SpeechModals = ({
   selectedSpeech,
   isViewModalOpen,
-  isEditModalOpen,
-  isDeleteAlertOpen,
   setIsViewModalOpen,
+  isEditModalOpen,
   setIsEditModalOpen,
+  isDeleteAlertOpen,
   setIsDeleteAlertOpen,
-  editTitle,
-  editContent,
-  setEditTitle,
-  setEditContent,
-  onSaveEdit,
-  onConfirmDelete,
   onEditClick
 }: SpeechModalsProps) => {
+  const { updateSpeech, deleteSpeech, fetchSpeeches } = useAuth();
+  const [editTitle, setEditTitle] = useState('');
+  const [editContent, setEditContent] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  
+  useEffect(() => {
+    if (selectedSpeech && isEditModalOpen) {
+      setEditTitle(selectedSpeech.title);
+      setEditContent(selectedSpeech.content);
+    }
+  }, [selectedSpeech, isEditModalOpen]);
+  
+  const handleUpdateSpeech = async () => {
+    if (!selectedSpeech) return;
+    
+    setIsSubmitting(true);
+    try {
+      await updateSpeech(selectedSpeech.id, editTitle, editContent);
+      await fetchSpeeches();
+      setIsEditModalOpen(false);
+      toast.success('Speech updated successfully');
+    } catch (error) {
+      console.error('Error updating speech:', error);
+      toast.error('Failed to update speech');
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+  
+  const handleDeleteSpeech = async () => {
+    if (!selectedSpeech) return;
+    
+    setIsSubmitting(true);
+    try {
+      await deleteSpeech(selectedSpeech.id);
+      await fetchSpeeches();
+      setIsDeleteAlertOpen(false);
+      toast.success('Speech deleted successfully');
+    } catch (error) {
+      console.error('Error deleting speech:', error);
+      toast.error('Failed to delete speech');
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+  
   return (
     <>
-      <ViewSpeechModal 
-        isOpen={isViewModalOpen}
-        onOpenChange={setIsViewModalOpen}
-        speech={selectedSpeech}
-        onEditClick={onEditClick}
-      />
-      
-      <EditSpeechModal 
-        isOpen={isEditModalOpen}
-        onOpenChange={setIsEditModalOpen}
-        speech={selectedSpeech}
-        editTitle={editTitle}
-        editContent={editContent}
-        setEditTitle={setEditTitle}
-        setEditContent={setEditContent}
-        onSave={onSaveEdit}
-      />
-      
-      <DeleteSpeechAlert 
-        isOpen={isDeleteAlertOpen}
-        onOpenChange={setIsDeleteAlertOpen}
-        speech={selectedSpeech}
-        onConfirm={onConfirmDelete}
-      />
+      {selectedSpeech && (
+        <>
+          <ViewSpeechModal
+            speech={selectedSpeech}
+            isOpen={isViewModalOpen}
+            setIsOpen={setIsViewModalOpen}
+            onEditClick={onEditClick}
+          />
+          
+          <EditSpeechModal
+            speech={selectedSpeech}
+            isOpen={isEditModalOpen}
+            setIsOpen={setIsEditModalOpen}
+            title={editTitle}
+            setTitle={setEditTitle}
+            content={editContent}
+            setContent={setEditContent}
+            onSave={handleUpdateSpeech}
+            isSubmitting={isSubmitting}
+          />
+          
+          <DeleteSpeechAlert
+            speech={selectedSpeech}
+            isOpen={isDeleteAlertOpen}
+            setIsOpen={setIsDeleteAlertOpen}
+            onDelete={handleDeleteSpeech}
+            isSubmitting={isSubmitting}
+          />
+        </>
+      )}
     </>
   );
 };
