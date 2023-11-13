89bb6a0d6e94d0106350f282e1575d2b8b1e4f15
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-11-13 20:11:21 +0600

    Fix: Improve settings and help page navigation

--- src/pages/HelpSupport.tsx
+++ src/pages/HelpSupport.tsx
@@ -10,35 +10,11 @@ import FAQsTab from '@/components/help/FAQsTab';
 import GuidesTab from '@/components/help/GuidesTab';
 import ContactTab from '@/components/help/ContactTab';
 import ResourcesTab from '@/components/help/ResourcesTab';
-import { ButtonCustom } from '@/components/ui/button-custom';
 
 const HelpSupport = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const { currentLanguage } = useLanguage();
   const { t } = useTranslation();
-
-  const faqs = [
-    {
-      question: "How do I create my first speech?",
-      answer: "Navigate to the Speech Lab from your dashboard. Click on 'Create New Speech', select the occasion, fill in the speech details, and follow the guided process to generate your speech."
-    },
-    {
-      question: "Can I edit my generated speeches?",
-      answer: "Yes! After generating a speech, you'll be taken to the editor where you can modify any part of your speech. Your changes will be saved automatically."
-    },
-    {
-      question: "How do I share my speech with others?",
-      answer: "From the 'My Speeches' section, find the speech you want to share, click on the share icon, and you can either copy a shareable link or directly send it via email."
-    },
-    {
-      question: "What subscription plans do you offer?",
-      answer: "We offer various subscription plans including Free, Premium, and Professional. Visit our Pricing page to see detailed features and benefits of each plan."
-    },
-    {
-      question: "How do I cancel my subscription?",
-      answer: "You can cancel your subscription anytime by going to Settings > Subscription and clicking on 'Cancel Subscription'. Your account will remain active until the end of the billing period."
-    }
-  ];
 
   return (
     <div className="min-h-screen flex">
@@ -47,8 +23,8 @@ const HelpSupport = () => {
       <div className="flex-1 bg-gray-50 overflow-auto">
         <div className="max-w-6xl mx-auto px-4 py-8">
           <div className="mb-8">
-            <h1 className="text-3xl font-bold text-gray-900">Help & Support</h1>
-            <p className="text-gray-600 mt-2">Find answers to common questions or contact our support team</p>
+            <h1 className="text-2xl font-bold text-gray-900">Help & Support</h1>
+            <p className="text-gray-600 mt-1">Find answers to common questions or contact our support team</p>
           </div>
 
           <div className="mb-8">
@@ -88,7 +64,7 @@ const HelpSupport = () => {
             </TabsList>
 
             <TabsContent value="faq" className="space-y-6">
-              <FAQsTab faqs={faqs} />
+              <FAQsTab />
             </TabsContent>
 
             <TabsContent value="guides" className="space-y-6">
