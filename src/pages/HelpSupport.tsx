b56af26344cdc494f08e80f24b8031b36831f801
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-11-14 23:33:12 +0600

    Fix: Improve tab bar appearance

--- src/pages/HelpSupport.tsx
+++ src/pages/HelpSupport.tsx
@@ -42,7 +42,7 @@ const HelpSupport = () => {
 
   return (
     <SpeechLabLayout>
-      <div className="max-w-6xl mx-auto px-4 py-8 mt-2">
+      <div className="max-w-6xl mx-auto px-4 py-8 mt-4">
         <div className="mb-8">
           <h1 className="text-2xl font-bold text-gray-900">Help & Support</h1>
           <p className="text-gray-600 mt-1">Find answers to common questions or contact our support team</p>
@@ -53,36 +53,38 @@ const HelpSupport = () => {
         </div>
 
         <Tabs defaultValue="faq" className="space-y-6">
-          <TabsList className="grid grid-cols-4 w-full max-w-2xl bg-gradient-to-r from-pink-500 via-pink-500 to-purple-600 p-1 rounded-lg">
-            <TabsTrigger 
-              value="faq" 
-              className="flex flex-col items-center gap-1 py-2 data-[state=active]:bg-white data-[state=active]:text-pink-600 data-[state=active]:shadow-md rounded-md transition-all"
-            >
-              <HelpCircle className="h-5 w-5" />
-              <span>FAQs</span>
-            </TabsTrigger>
-            <TabsTrigger 
-              value="guides" 
-              className="flex flex-col items-center gap-1 py-2 data-[state=active]:bg-white data-[state=active]:text-pink-600 data-[state=active]:shadow-md rounded-md transition-all"
-            >
-              <BookOpen className="h-5 w-5" />
-              <span>Guides</span>
-            </TabsTrigger>
-            <TabsTrigger 
-              value="contact" 
-              className="flex flex-col items-center gap-1 py-2 data-[state=active]:bg-white data-[state=active]:text-pink-600 data-[state=active]:shadow-md rounded-md transition-all"
-            >
-              <MessageSquare className="h-5 w-5" />
-              <span>Contact Us</span>
-            </TabsTrigger>
-            <TabsTrigger 
-              value="resources" 
-              className="flex flex-col items-center gap-1 py-2 data-[state=active]:bg-white data-[state=active]:text-pink-600 data-[state=active]:shadow-md rounded-md transition-all"
-            >
-              <FileText className="h-5 w-5" />
-              <span>Resources</span>
-            </TabsTrigger>
-          </TabsList>
+          <div className="bg-white p-4 rounded-lg shadow-sm">
+            <TabsList className="grid grid-cols-4 w-full max-w-2xl bg-gradient-to-r from-pink-500 via-pink-500 to-purple-600 p-1 rounded-lg">
+              <TabsTrigger 
+                value="faq" 
+                className="flex flex-col items-center gap-1 py-2 data-[state=active]:bg-white data-[state=active]:text-pink-600 data-[state=active]:shadow-md rounded-md transition-all"
+              >
+                <HelpCircle className="h-5 w-5" />
+                <span>FAQs</span>
+              </TabsTrigger>
+              <TabsTrigger 
+                value="guides" 
+                className="flex flex-col items-center gap-1 py-2 data-[state=active]:bg-white data-[state=active]:text-pink-600 data-[state=active]:shadow-md rounded-md transition-all"
+              >
+                <BookOpen className="h-5 w-5" />
+                <span>Guides</span>
+              </TabsTrigger>
+              <TabsTrigger 
+                value="contact" 
+                className="flex flex-col items-center gap-1 py-2 data-[state=active]:bg-white data-[state=active]:text-pink-600 data-[state=active]:shadow-md rounded-md transition-all"
+              >
+                <MessageSquare className="h-5 w-5" />
+                <span>Contact Us</span>
+              </TabsTrigger>
+              <TabsTrigger 
+                value="resources" 
+                className="flex flex-col items-center gap-1 py-2 data-[state=active]:bg-white data-[state=active]:text-pink-600 data-[state=active]:shadow-md rounded-md transition-all"
+              >
+                <FileText className="h-5 w-5" />
+                <span>Resources</span>
+              </TabsTrigger>
+            </TabsList>
+          </div>
 
           <TabsContent value="faq" className="space-y-6">
             <FAQsTab faqs={faqs} />
