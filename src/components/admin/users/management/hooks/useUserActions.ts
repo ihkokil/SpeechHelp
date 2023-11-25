71beeedb6f3673bc1a5efead02d22654c7245cca
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2023-11-25 15:12:12 +0600

    Refactor: Split useUserActions hook

--- src/components/admin/users/management/hooks/useUserActions.ts
+++ src/components/admin/users/management/hooks/useUserActions.ts
@@ -1,185 +1,45 @@
 
-import { useState, useCallback } from 'react';
-import { User } from '../../types';
-import { useToast } from '@/hooks/use-toast';
+import { useActionState } from './user-actions/useActionState';
+import { useUserCrud } from './user-actions/useUserCrud';
+import { useSubscriptionActions } from './user-actions/useSubscriptionActions';
+import { useUserDetails } from './user-actions/useUserDetails';
+import { usePermissionActions } from './user-actions/usePermissionActions';
 
 export const useUserActions = () => {
-  const [isActionLoading, setIsActionLoading] = useState(false);
-  const [selectedUser, setSelectedUser] = useState<User | null>(null);
-  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
-  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
-  const [isAddUserDialogOpen, setIsAddUserDialogOpen] = useState(false);
-  const [isPermissionsDialogOpen, setIsPermissionsDialogOpen] = useState(false);
-  const { toast } = useToast();
+  const {
+    isActionLoading,
+    setIsActionLoading,
+    selectedUser,
+    setSelectedUser,
+    isDetailsOpen,
+    setIsDetailsOpen,
+    isDeleteDialogOpen,
+    setIsDeleteDialogOpen,
+    isAddUserDialogOpen,
+    setIsAddUserDialogOpen,
+    isPermissionsDialogOpen,
+    setIsPermissionsDialogOpen,
+    reset
+  } = useActionState();
 
-  // Reset all state
-  const reset = useCallback(() => {
-    setIsActionLoading(false);
-    setSelectedUser(null);
-    setIsDetailsOpen(false);
-    setIsDeleteDialogOpen(false);
-    setIsAddUserDialogOpen(false);
-    setIsPermissionsDialogOpen(false);
-  }, []);
+  const {
+    handleDeleteUsers,
+    handleToggleUserStatus
+  } = useUserCrud(setIsActionLoading, setIsDeleteDialogOpen);
 
-  const handleDeleteUsers = useCallback(async (selectedUsers: string[], users: User[], setUsers: (users: User[]) => void) => {
-    console.log('Deleting users:', selectedUsers);
-    if (selectedUsers.length === 0) {
-      toast({
-        title: 'No users selected',
-        description: 'Please select at least one user to delete.',
-        variant: 'destructive',
-      });
-      return false;
-    }
+  const {
+    handleToggleUserSubscription
+  } = useSubscriptionActions(setIsActionLoading);
 
-    setIsActionLoading(true);
-    try {
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      
-      setUsers(users.filter(user => !selectedUsers.includes(user.id)));
-      
-      toast({
-        title: 'Success',
-        description: `${selectedUsers.length} users have been deleted.`,
-      });
+  const {
+    handleViewUserDetails,
+    handleCloseUserDetails,
+    handleManagePermissions
+  } = useUserDetails(setSelectedUser, setIsDetailsOpen, setIsPermissionsDialogOpen);
 
-      setIsDeleteDialogOpen(false);
-      return true;
-    } catch (error) {
-      console.error('Error deleting users:', error);
-      toast({
-        title: 'Error',
-        description: 'Failed to delete users.',
-        variant: 'destructive',
-      });
-      return false;
-    } finally {
-      setIsActionLoading(false);
-    }
-  }, [toast]);
-
-  const handleToggleUserStatus = useCallback(async (userId: string, isActive: boolean, users: User[], setUsers: (users: User[]) => void) => {
-    console.log('Toggling user status:', userId, isActive);
-    setIsActionLoading(true);
-    try {
-      await new Promise(resolve => setTimeout(resolve, 500));
-      
-      setUsers(
-        users.map(user => 
-          user.id === userId ? { ...user, is_active: isActive } : user
-        )
-      );
-      
-      toast({
-        title: 'Success',
-        description: `User status updated to ${isActive ? 'active' : 'inactive'}.`,
-      });
-      return true;
-    } catch (error) {
-      console.error('Error updating user status:', error);
-      toast({
-        title: 'Error',
-        description: 'Failed to update user status.',
-        variant: 'destructive',
-      });
-      return false;
-    } finally {
-      setIsActionLoading(false);
-    }
-  }, [toast]);
-
-  const handleViewUserDetails = useCallback((user: User) => {
-    console.log('UserManagement: Opening details for user:', user.id);
-    
-    // Set the selected user first
-    setSelectedUser(user);
-    
-    // Then open the drawer
-    setIsDetailsOpen(true);
-  }, []);
-
-  const handleCloseUserDetails = useCallback(() => {
-    console.log('UserManagement: Closing user details drawer');
-    
-    // Close the drawer first
-    setIsDetailsOpen(false);
-    
-    // Clear the selected user after a short delay to avoid state conflicts
-    setTimeout(() => {
-      setSelectedUser(null);
-    }, 300);
-  }, []);
-
-  const handleToggleUserSubscription = useCallback(async (userId: string, extensionDays: number = 30, users: User[], setUsers: (users: User[]) => void) => {
-    console.log('Extending subscription for user:', userId, 'by', extensionDays, 'days');
-    setIsActionLoading(true);
-    try {
-      await new Promise(resolve => setTimeout(resolve, 800));
-      
-      setUsers(
-        users.map(user => {
-          if (user.id === userId) {
-            const currentEndDate = user.subscription_end_date 
-              ? new Date(user.subscription_end_date) 
-              : new Date();
-            
-            currentEndDate.setDate(currentEndDate.getDate() + extensionDays);
-            
-            return { 
-              ...user, 
-              subscription_status: 'active',
-              subscription_end_date: currentEndDate.toISOString() 
-            };
-          }
-          return user;
-        })
-      );
-      
-      toast({
-        title: 'Success',
-        description: `User subscription extended by ${extensionDays} days.`,
-      });
-      return true;
-    } catch (error) {
-      console.error('Error updating subscription:', error);
-      toast({
-        title: 'Error',
-        description: 'Failed to update user subscription.',
-        variant: 'destructive',
-      });
-      return false;
-    } finally {
-      setIsActionLoading(false);
-    }
-  }, [toast]);
-
-  const handleManagePermissions = useCallback((user: User) => {
-    console.log('UserManagement: Opening permissions dialog for user:', user.id);
-    
-    // Set the selected user
-    setSelectedUser(user);
-    
-    // Open the permissions dialog
-    setIsPermissionsDialogOpen(true);
-  }, []);
-
-  const handlePermissionsUpdated = useCallback((updatedUser: User, users: User[], setUsers: (users: User[]) => void) => {
-    console.log('Permissions updated for user:', updatedUser.id);
-    setUsers(
-      users.map(user => 
-        user.id === updatedUser.id ? updatedUser : user
-      )
-    );
-    
-    toast({
-      title: 'Permissions Updated',
-      description: `${updatedUser.email}'s admin permissions have been updated.`,
-    });
-    
-    // Close the dialog
-    setIsPermissionsDialogOpen(false);
-  }, [toast]);
+  const {
+    handlePermissionsUpdated
+  } = usePermissionActions(setIsPermissionsDialogOpen);
 
   return {
     isActionLoading,
