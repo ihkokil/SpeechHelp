55a4484fe8761fd706a3e9ac456f687c00654ffb
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2024-03-09 09:50:59 +0600

    Refactor: Split French translations into sections

---
+++ src/translations/fr/navigation.ts
@@ -0,0 +1,8 @@
+
+export const navigation = {
+  'nav.home': 'Accueil',
+  'nav.features': 'Fonctionnalités',
+  'nav.howItWorks': 'Comment ça marche',
+  'nav.testimonials': 'Témoignages',
+  'nav.contact': 'Contact',
+};
