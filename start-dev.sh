4e1e3bf46b4e0e4857aad50d63f67e50429516bf
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2025-04-29 22:54:50 +0000

    Fix: Development server start error
    The development server was failing to start due to the 'vite' command not being found. This commit addresses the issue.

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,13 @@
 
 #!/bin/bash
 # This script runs the Node.js script to start the development server
+
+# Make the Node.js script executable
 chmod +x start-dev.js
-node start-dev.js
+
+# Run with direct path to node if possible, otherwise use the 'node' command
+if [ -f /usr/bin/node ]; then
+  /usr/bin/node start-dev.js
+else
+  node start-dev.js
+fi
