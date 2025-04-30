a3d52a3a66bb86f72ceb4abe8967351cbc9527c2
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2025-04-30 00:06:51 +0000

    Reverted to commit 687e3fedbd4c541c499c3f14115879f9a0896812

--- start-dev.sh
+++ start-dev.sh
@@ -1,20 +1,4 @@
 
 #!/bin/bash
-
-# Check if node is installed
-if ! command -v node &> /dev/null; then
-    echo "Node.js is not installed. Please install Node.js to run this script."
-    exit 1
-fi
-
-# Make sure the script itself is executable
-chmod +x "$0"
-
-# Check if dependencies are installed
-if [ ! -d "node_modules" ]; then
-    echo "Installing dependencies..."
-    npm install
-fi
-
-# Run the JavaScript script that handles Vite startup
-node start-dev.js "$@"
+# This script runs the Node.js script to start the development server
+node start-dev.js
