14afe102819d38f86e725a70a6814d31e5132662
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2025-05-02 08:54:02 +0600

    Remove speech display from user details drawer

--- src/components/admin/users/details/DrawerContent.tsx
+++ src/components/admin/users/details/DrawerContent.tsx
@@ -6,19 +6,13 @@ import { DrawerSheetContent } from './DrawerSheetContent';
 interface DrawerContentProps {
   user: User;
   onClose: () => void;
-  speeches: any[];
-  isLoadingSpeeches: boolean;
   userJoinedDays: number;
-  totalActivityTime: number;
 }
 
 export const DrawerContent: React.FC<DrawerContentProps> = ({
   user,
   onClose,
-  speeches,
-  isLoadingSpeeches,
-  userJoinedDays,
-  totalActivityTime
+  userJoinedDays
 }) => {
   // Wrap onClose in a function that accepts an event to prevent immediate closure
   const handleClose = (e: React.MouseEvent) => {
@@ -33,10 +27,7 @@ export const DrawerContent: React.FC<DrawerContentProps> = ({
     <DrawerSheetContent
       user={user}
       onClose={handleClose}
-      speeches={speeches}
-      isLoadingSpeeches={isLoadingSpeeches}
       userJoinedDays={userJoinedDays}
-      totalActivityTime={totalActivityTime}
     />
   );
 };
