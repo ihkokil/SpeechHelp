852b427532217cb53953a64be237292c1b29ef01
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2025-06-03 21:06:33 +0600

    DB Changes

--- src/pages/AdminAuth.tsx
+++ src/pages/AdminAuth.tsx
@@ -15,7 +15,7 @@ import { AlertCircle, LockKeyhole, Shield, Info } from 'lucide-react';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { supabase } from '@/integrations/supabase/client';
 
-const logoPath = "https://yotrueuqjxmgcwlbbyps.supabase.co/storage/v1/object/public/svg_files//Speech%20Help%20Logo.svg";
+const logoPath = "SpeechHelp_Logo.svg";
 
 const loginSchema = z.object({
   username: z.string().min(1, 'Username or email is required'),
