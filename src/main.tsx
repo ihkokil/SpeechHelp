d3032138c6f06140207947bebfbe8414cfbe9589
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2025-10-02 18:09:39 +0600

    Refactor: Implement screen scaling logic

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { initializeScaling } from './utils/screenScaling'
+
+// Initialize dynamic scaling system
+initializeScaling();
 
 createRoot(document.getElementById("root")!).render(<App />);
